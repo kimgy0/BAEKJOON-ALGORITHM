90436fa24ef9e36ef1c78a1b0d8a13aa4bb24996
01086055223@hanmail.net <01086055223@hanmail.net>
2022-06-02 11:18:41 +0900

    add : 인프런 1-2 /  주석추가

--- inflearn/Solve1_2.java
+++ inflearn/Solve1_2.java
@@ -15,6 +15,14 @@ public class Solve1_2 {
             }else{
                 input[i] = Character.toUpperCase(input[i]);
             }
+            /**
+             *  아스키코드로 하는 방법
+             *  A -> 65 ~ 90
+             *  a -> 97 ~ 122
+             *
+             *  소문자 아스키에 32 를 빼면 소문자 아스키 값이 출력된다.
+             *  자료형은 (char) 로 묶어주자.
+             */
             System.out.print(input[i]);
         }
     }
